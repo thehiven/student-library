a8ef0535963fee2a00b747a8cfbfc71cb1a5f688
hiven <vladyslavnadtoka@gmail.com>
2018-10-27 22:04:29 -0700

    added search feature

--- js/script.js
+++ js/script.js
@@ -1,53 +1,8 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Author: Vlad 'hiven' Nadtoka
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
 
 function createPagination(page, listLength) {
   const div = document.createElement('div');
@@ -56,6 +11,19 @@ function createPagination(page, listLength) {
   const ul = document.createElement('ul');
   div.appendChild(ul);
 
+  fillPagination(listLength, ul);
+  page.appendChild(div);
+
+  return ul;
+}
+
+function clearPagination(pagination) {
+  while (pagination.firstChild) {
+    pagination.removeChild(pagination.firstChild);
+  }
+}
+
+function fillPagination(listLength, pagination) {
   const numberOfPages = Math.ceil(listLength / 10);
   for (let i = 0; i < numberOfPages; i++) {
     const li = document.createElement('li');
@@ -64,11 +32,26 @@ function createPagination(page, listLength) {
     a.textContent = i + 1;
 
     li.appendChild(a);
-    ul.appendChild(li);
+    pagination.appendChild(li);
   }
-  page.appendChild(div);
+}
 
-  return ul;
+function createSearch(pageHeader) {
+  const div = document.createElement('div');
+  div.className = 'student-search';
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.placeholder = 'Search for students...';
+
+  const button = document.createElement('button');
+  button.textContent = 'Search';
+
+  div.appendChild(input);
+  div.appendChild(button);
+  pageHeader.appendChild(div);
+
+  return div;
 }
 
 function showPage(list, pageNumber) {
@@ -84,10 +67,31 @@ function showPage(list, pageNumber) {
   }
 }
 
+function showSearchResults(list, searchValue, pagination) {
+  clearPagination(pagination);
+
+  let foundStudents = [];
+  for (let i = 0; i < list.length; i++) {
+    let name = list[i].getElementsByTagName('h3')[0].textContent;
+    if (name.includes(searchValue)) {
+      foundStudents.push(list[i]);
+      if (list[i].style.display != 'block') list[i].style.display = 'block';
+    } else {
+      list[i].style.display = 'none';
+    }
+  }
+
+  fillPagination(foundStudents.length, pagination);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('div.page');
+  const pageHeader = document.querySelector('div.page-header')
   const students = document.querySelectorAll('li.student-item');
   const pagination = createPagination(page, students.length);
+  const search = createSearch(pageHeader);
+  const searchInput = search.getElementsByTagName('input')[0];
+  const searchButton = search.getElementsByTagName('button')[0];
 
   let previousActiveLink = pagination.firstChild.firstChild;
   previousActiveLink.className = 'active';
@@ -98,5 +102,13 @@ document.addEventListener('DOMContentLoaded', () => {
     previousActiveLink = e.target;
     e.target.className = 'active';
     showPage(students, parseInt(e.target.textContent));
+  });
+
+  searchButton.addEventListener('click', () => {
+    showSearchResults(students, searchInput.value, pagination);
+  });
+
+  searchInput.addEventListener('keyup', () => {
+    showSearchResults(students, searchInput.value, pagination);
   });
 });
